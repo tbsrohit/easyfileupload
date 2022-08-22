4c0e1c58d2a717191b7fb3261dd7786dbd30fe71
Rohit <tbs.rohitj@gmail.com>
2022-08-22 12:06:16 +0530

    First commint

---
+++ src/Fileupload.php
@@ -0,0 +1,32 @@
+<?php
+namespace Easyfileupload\Easyfileupload;
+/***
+ * 
+ * Class Fileupload
+ * 
+ * @author Rohit jayswal <tbs.rohitj@gmail.com>
+ */
+class Fileupload{
+    public static $temFile;
+    public static $filePath;
+    public static $fileName;
+    public static $orgFileName;
+    public static function upload($file, $path){
+        static::$temFile = $file;
+        static::$filePath = __DIR__.'/'.$path;
+        return new static;
+    }
+    public function save(){
+        $fileName = time() . uniqid() . '.' . pathinfo(static::$temFile, PATHINFO_EXTENSION);
+        static::$orgFileName = pathinfo(static::$temFile, PATHINFO_BASENAME);
+        static::$fileName = $fileName;
+        static::$temFile->move(static::$filePath, $fileName);
+        return new static;
+    }
+    public function getFileName(){
+        return static::$fileName;
+    }
+    public function getOrgFileName(){
+        return static::$orgFileName;
+    }
+}
